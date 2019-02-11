f8f5a9f80974884825fb88f362925f07953b1315
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2019-02-11 22:30:02 +0100

    lyd og js i separat fil

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,34 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init);
+
+const dest = document.querySelector("#typewriter");
+let text = document.querySelector("#typewriter").textContent;
+let counter;
+
+function init() {
+  console.log("init");
+  counter = 0;
+  dest.textContent = "";
+  loop();
+}
+function loop() {
+  console.log("loop");
+  if (text.length > counter) {
+    //find text from 0 to counter with substring
+    counter++;
+    let textLength = text.substring(0, counter);
+    dest.textContent = textLength;
+
+    var lastChar = textLength[textLength.length - 1];
+    console.log(lastChar);
+
+    if (lastChar == " ") {
+      document.querySelector("#typespace").play();
+    } else {
+      document.querySelector("#typekey1").play();
+    }
+    console.log(counter);
+    console.log(textLength);
+    setTimeout(loop, 400);
+  }
+}
