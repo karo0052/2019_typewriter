b05eb11bcaadadc366a0a16311f37906deedea6b
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2019-02-12 08:46:34 +0100

    kommentarer tilfoejet

--- typewriter.js
+++ typewriter.js
@@ -14,21 +14,25 @@ function init() {
 function loop() {
   console.log("loop");
   if (text.length > counter) {
-    //find text from 0 to counter with substring
+    //counter +1
     counter++;
+    //find text from 0 to counter with substring
     let textLength = text.substring(0, counter);
+    //insert in HTML
     dest.textContent = textLength;
-
+    //find the last character in the text
     var lastChar = textLength[textLength.length - 1];
     console.log(lastChar);
-
+    //if the last character is space, then play this sound
     if (lastChar == " ") {
       document.querySelector("#typespace").play();
+      //else play this sound
     } else {
       document.querySelector("#typekey1").play();
     }
     console.log(counter);
     console.log(textLength);
+    //set a delay at 400ms
     setTimeout(loop, 400);
   }
 }
